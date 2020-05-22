7fe42ea1746758a9f7a6d879bbeaef707ed2d398
Rogério de Andrade Pires <rogeriodeandradepires@live.com>
2020-05-22 16:00:28 -0300

    Initial commit

---
+++ standard_backend_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PizzariaStandardWsProjectConfig(AppConfig):
+    name = 'standard_backend_app'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\roger\Desktop\flutter_projects\django\pizzeria_standard_ws\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
